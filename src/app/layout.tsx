4dd5d3d9ec7c850612c5f34b390528becd1504d0
grzeli <grzelak.szymon@gmail.com>
2023-02-27 13:54:24 +0100

    added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Header from '@/components/Header'
 import './globals.css'
+import Providers from './Providers'
 
 export default function RootLayout({
   children,
@@ -7,12 +9,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
