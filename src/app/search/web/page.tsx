b97b444dacaa7ba0e9cec532ca06c6376355254f
grzeli <grzelak.szymon@gmail.com>
2023-03-02 13:50:11 +0100

    added error component && UI for missing search results

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,11 +1,29 @@
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import Link from 'next/link';
 import React from 'react';
 import { GoogleResponseT, GoogleResultObjectT } from './types';
 
 export default async function WebSearchPage({ searchParams }: Params) {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=
     ${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CX_KEY}&q=${searchParams.searchTerm}`);
+
+  if (!response.ok) {
+    throw new Error('Ups...Something went wrong. Please try again');
+  }
+
   const data: GoogleResponseT = await response.json();
+
+  if (!data?.items) {
+    return (
+      <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>No results found</h1>
+        <p className='text-lg '>Try searching something else or check your spelling</p>
+        <Link className='text-blue-500 pt-8' href='/'>
+          Go to home page
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <>
