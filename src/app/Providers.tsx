595f129f51c753cf039e5549a0b1b0ecd311f4fa
grzeli <grzelak.szymon@gmail.com>
2023-03-03 11:46:17 +0100

    removed darkMode

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -9,7 +9,7 @@ type PropsT = {
 
 const Providers: React.FC<PropsT> = ({ children }) => {
   return (
-    <ThemeProvider enableSystem={true} attribute={'class'}>
+    <ThemeProvider attribute={'class'}>
       <div className='dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen select-none'>
         {children}
       </div>
