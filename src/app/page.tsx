ec6d492e92a5f992a313acbaf00e129da3d4a537
grzeli <grzelak.szymon@gmail.com>
2023-03-06 11:48:30 +0100

    mobile UI adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <>
       <HomeHeader />
-      <div className='flex flex-col items-center mt-24'>
+      <div className='flex flex-col items-center mt-4 sm:mt-24'>
         <Image
           width={'290'}
           height={'100'}
           src={'/home_page_logo.png'}
           alt={'google'}
-          className='rounded-3xl shadow-xl object-contain'
+          className='rounded-3xl shadow-xl object-contain w-36 sm:w-[200px] lg:w-[300px]'
         ></Image>
         <HomeSearch />
       </div>
